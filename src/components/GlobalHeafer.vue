0dc7939c0d5a03b38c1d628d07dd19ec60fb6b1c
yaoyuxian2023 <13414061936@163.com>
2024-07-02 08:09:08 +0800

    前端初始化项目

---
+++ src/components/GlobalHeafer.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="globalHeader">
+    <a-menu
+      mode="horizontal"
+      :selected-keys="selectedKeys"
+      @menu-item-click="doMenuClick"
+    >
+      <a-menu-item
+        key="0"
+        :style="{ padding: 0, marginRight: '38px' }"
+        disabled
+      >
+        <div class="table-bar">
+          <img class="logo" src="../assets/stack.svg" />
+          <div class="title">鱼 OJ</div>
+        </div>
+      </a-menu-item>
+      <a-menu-item v-for="item in routes" :key="item.path">
+        {{ item.name }}
+      </a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { useRoute, useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+// 默认主页
+const selectedKeys = ref(["/"]);
+
+// 路由跳转时，更新选中的菜单栏
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.table-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: #444;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="basiclayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobalHeafer />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://yupi.icu" target="_blank">
+          编程题目在线测评系统 by 姚禹娴
+        </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+#basiclayout {
+}
+
+#basiclayout .header {
+  background: red;
+  margin-bottom: 16px;
+}
+
+#basiclayout .content {
+  background: linear-gradient(to right, #bbb, #fff);
+  margin-bottom: 16px;
+}
+
+#basiclayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+<script setup lang="ts">
+import GlobalHeafer from "@/components/GlobalHeafer.vue";
+</script>
